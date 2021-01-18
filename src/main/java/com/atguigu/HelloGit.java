2e31bb24b4d1afe983ad428526ebcb840a4f2b64
lixw <lixuwei0917@163.com>
2021-01-18 21:46:19 +0800

    测试Hello git

---
+++ src/main/java/com/atguigu/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author lixw
+ * @create 2021-01-18 21:44
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("11111111111111");
+    }
+}
