f1aec1b098e7a69e346e2beab61cae1bfcf0bed5
lixw <lixuwei0917@163.com>
2021-01-18 21:52:26 +0800

    aaaaaaaaaaaaaa

--- src/main/java/com/atguigu/HelloGit.java
+++ src/main/java/com/atguigu/HelloGit.java
@@ -7,5 +7,6 @@ package com.atguigu;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("11111111111111");
+        System.out.println("aaaaaaaaaaaaaa");
     }
 }
